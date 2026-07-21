e84aec40895328716b0c889887fa3418530c4d9a
pradeeptodixit <pradeeptodixit9@gmail.com>
2026-07-21 20:43:17 +0530

    JavaScript

---
+++ es6.js
@@ -0,0 +1,20 @@
+// JavaScript
+// Function: block of code
+// Syntax
+// function fname(){
+// block of code
+// }
+// function calling{}
+// passing parameters in function
+function sayHello(a, b) {
+  console.log("CSE-24 FSD");
+  console.log("Arguments");
+  return a + b;
+}
+console.log(sayHello(3, 2));
+// Arrow function
+// ()=>{}
+const sayhello = () => {
+  console.log("Arrow Function");
+};
+sayhello();
