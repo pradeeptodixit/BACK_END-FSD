1a5d2f755b729271878c86313646aeeb06c890f3
pradeeptodixit <pradeeptodixit9@gmail.com>
2026-09-24 15:25:24 +0530

    Backend

---
+++ Javascript/program2/script.js
@@ -0,0 +1,12 @@
+let x,y;
+x=4;
+y=6;
+
+age=25;
+console.log(age);
+
+let gpa=10.0;
+console.log(gpa)
+
+console.log(`You are ${age} years old.`);
+console.log(`Nitin got ${gpa} gpa in first semester`)
