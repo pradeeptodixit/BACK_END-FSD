92f1463b85f9a07897ac9f5152fa8242f87a3819
pradeeptodixit <pradeeptodixit9@gmail.com>
2026-09-25 02:57:30 +0530

    Backend

---
+++ Javascript/program2/script.js
@@ -0,0 +1,16 @@
+// let x,y;
+// x=4;
+// y=6;
+
+// age=3;
+// console.log(age);
+
+// let gpa=10.0;
+// console.log(gpa)
+
+// console.log(typeof(age))
+// console.log(`You are ${age} years old.`);
+// console.log(`Nitin got ${gpa} gpa in first semester`)
+
+let fav="pizza";
+console.log(`My favorite fast food is ${fav}`)
